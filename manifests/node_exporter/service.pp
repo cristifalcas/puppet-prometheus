d235a2f7f70a845afc45758c14a6272f8fdb6bf3
Cristian Falcas <falcas@optymyze.com>
2017-02-02 18:00:00 +0200

    don't start service in case the package is ensured absent

--- manifests/node_exporter/service.pp
+++ manifests/node_exporter/service.pp
@@ -1,8 +1,16 @@
 # == Class prometheus::node_exporter::service
 #
 class prometheus::node_exporter::service {
+  if $::prometheus::node_exporter::package_ensure == 'absent' {
+    $service_ensure ='stopped'
+    $service_enable = false 
+  } else {
+    $service_ensure ='running'
+    $service_enable = true 
+  }
+
   service { 'node_exporter':
-    ensure => 'running',
-    enable => true,
+    ensure => $service_ensure,
+    enable => $service_enable,
   }
 }
