d9cbf46489c0175349feb0c450b534c71d49b543
olevole <olevole@olevole.ru>
2017-02-23 02:29:26 +0300

    for pull #3: Forget to add FreeBSD rc.d script

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,6 +12,22 @@ class prometheus::config {
           content => template("${module_name}/sysconfig/prometheus"),
         }
       }
+      'FreeBSD': {
+        file { '/etc/rc.conf.d/prometheus':
+          ensure  => 'file',
+          mode    => '0644',
+          owner   => $::prometheus::user,
+          group   => $::prometheus::group,
+          content => template("${module_name}/freebsd/prometheus"),
+        }
+        file { '/usr/local/etc/rc.d/prometheus':
+          ensure  => 'file',
+          mode    => '0555',
+          owner   => 'root',
+          group   => 'wheel',
+          content => template("${module_name}/freebsd/prometheus_rcconf.erb"),
+        }
+      }
     }
   }
   if $::prometheus::manage_config {
